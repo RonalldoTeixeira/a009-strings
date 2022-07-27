97230722d26e3d9b86910ccc672bc88c32becbd6
RonalldoTeixeira <ronalldo.tb@gmail.com>
2022-07-26 21:00:20 -0300

    respostas

--- exercicio2/script.js
+++ exercicio2/script.js
@@ -1,1 +1,9 @@
 const minhaString = "Eu não sou supersticioso, mas sou um pouco ________.      ";
+console.log(minhaString)
+console.log("antes minha string tinha", minhaString.length, "caracteres.")
+
+const nS = minhaString.trim()
+console.log (`${nS.replace("________" , "sticioso")}`)
+console.log(`Minha string depois de remover os espaços tem ${nS.length} caracterios.`)
+
+
